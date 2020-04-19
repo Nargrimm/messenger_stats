82b8fb222b0b319c7f03da1425c19263db37c6ae
Victor LE CORNEC <nargrimm@gmail.com>
2020-04-19 20:16:23 +0200

    Add: plotting and words occurence

--- stats.py
+++ stats.py
@@ -1,8 +1,15 @@
 import argparse
+import datetime
 import json
 import operator
 import os
 import sys
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+
 
 class Message:
     #if the content type is a pictue the content will be the path to this pic
@@ -13,6 +20,16 @@ class Message:
         self.content_type = content_type
         self.content = content
 
+    def get_message_hour(self):
+        return datetime.datetime.fromtimestamp(int(self.timestamp_ms)/1000.0).hour
+
+
+    def get_message_weekday(self):
+        return datetime.datetime.fromtimestamp(int(self.timestamp_ms)/1000.0).weekday()
+
+
+    def get_message_year(self):
+        return datetime.datetime.fromtimestamp(int(self.timestamp_ms)/1000.0).year
 
 class Conversation:
     def __init__(self, directory):
@@ -109,14 +126,123 @@ class Conversation:
         for p in self.participants:
             participants_pics[p] = 0
         for msg in self.messages:
-            if msg.content_type != "photos":
+            if msg.content_type != 'photos':
                 continue
             participants_pics[msg.sender] += len(msg.content.split(';')) - 1 
         return participants_pics
 
+    #Return a dict with hour, weekday and year repartition
+    #We do this in a single iteration which is faster than calling the 3 functions 
+    def get_message_time_repartition(self):
+        msg_per_hour = dict()
+        msg_per_weekday = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}
+        msg_per_year = dict()
+        for msg in self.messages:
+            hour = msg.get_message_hour()
+            if hour not in msg_per_hour:
+                msg_per_hour[hour] = 1
+            else:
+                msg_per_hour[hour] += 1
+            msg_per_weekday[str(msg.get_message_weekday())] += 1
+            year = msg.get_message_year()
+            if year not in msg_per_year:
+                msg_per_year[year] = 1
+            else:
+                msg_per_year[year] += 1
+        weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        for i in range(0, 7):
+            msg_per_weekday[weekdays[i]] = msg_per_weekday.pop(str(i))
+        return {'hour': msg_per_hour, 'weekday': msg_per_weekday, 'year': msg_per_year}
+
+    def get_number_of_messages_per_hour(self):
+        msg_per_hour = dict()
+        for msg in self.messages:
+            hour = msg.get_message_hour()
+            if hour not in msg_per_hour:
+                msg_per_hour[hour] = 1
+            else:
+                msg_per_hour[hour] += 1
+        return msg_per_hour
+
+
+    def get_number_of_messages_per_weekday(self):
+        msg_per_weekday = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}
+        for msg in self.messages:
+            msg_per_weekday[str(msg.get_message_weekday())] += 1
+        msg_per_weekday['Monday'] = msg_per_weekday.pop('0')
+        msg_per_weekday['Tuesday'] = msg_per_weekday.pop('1')
+        msg_per_weekday['Wednesday'] = msg_per_weekday.pop('2')
+        msg_per_weekday['Thursday'] = msg_per_weekday.pop('3')
+        msg_per_weekday['Friday'] = msg_per_weekday.pop('4')
+        msg_per_weekday['Saturday'] = msg_per_weekday.pop('5')
+        msg_per_weekday['Sunday'] = msg_per_weekday.pop('6')
+        return msg_per_weekday
+            
+
+    def get_number_of_messages_per_year(self):
+        msg_per_year = dict()
+        for msg in self.messages:
+            year = msg.get_message_year()
+            if year not in msg_per_year:
+                msg_per_year[year] = 1
+            else:
+                msg_per_year[year] += 1
+        return msg_per_year
+
+    def get_most_used_words(self, min_size, number_of_word):
+        words_occurence = {}
+        for msg in self.messages:
+            msg_txt = msg.content.encode("iso-8859-1").decode("utf8")
+            words = [x.strip() for x in msg_txt.split(' ')]
+            for word in words:
+                word = word.lower()
+                if len(word) == 0 or len(word) <= min_size:
+                    continue
+                words_occurence[word] = words_occurence.get(word, 0) + 1
+
+        top_words = sorted(words_occurence.items(), key=operator.itemgetter(1), reverse=True)[:number_of_word]
+        return top_words
+
 
 def print_dict_ordered_reverse(d):
     print(sorted(d.items(), key=operator.itemgetter(1), reverse=True))
+
+
+def create_bar_plot_from_list(l, x_name, y_name, name):
+    x = []
+    y = []
+    for item in l:
+        x.append(item[0])
+        y.append(item[1])
+    create_bar_plot(x, x_name, y, y_name, name)
+
+
+#TODO maybe merge all graph in a huge picture
+def create_bar_plot(x, x_name, y, y_name, name):
+    plt.figure(figsize=(16,11))
+    df = pd.DataFrame(list(zip(x, y)), columns=(x_name, y_name))
+    plot = sns.barplot(x=x_name, y=y_name, data=df, color='#3377ff')
+    plot.set_xticklabels(plot.get_xticklabels(), rotation=45, horizontalalignment='right')
+    plot.get_figure().savefig(name, dpi=300)
+
+
+def merge_pictures(images_path):
+    images = [Image.open(x) for x in images_path]
+    total_height = 0
+    total_width = 0
+
+    for img in images:
+        size = img.size
+        total_height += size[1]
+        total_width = max(size[0], total_width)
+
+    merge = Image.new('RGB', (total_width, total_height))
+    y_offset = 0
+    for img in images:
+        merge.paste(img, (0, y_offset))
+        y_offset += img.size[1]
+    
+    merge.save('merge.jpg')
 
 
 if __name__ == '__main__':
@@ -125,11 +251,34 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     print(args.directory)
+    
     conv = Conversation(args.directory)
+    words_occurence = conv.get_most_used_words(3, 100)
+    print(words_occurence)
 
-    print_dict_ordered_reverse(conv.number_of_messages_per_participants)
-    print_dict_ordered_reverse(conv.number_of_char_per_participants)
-    print_dict_ordered_reverse(conv.number_of_photos_per_participants)
+    msg_per_participant = sorted(conv.number_of_messages_per_participants.items(), key=operator.itemgetter(1), reverse=True)
+    #create_bar_plot_from_list(msg_per_participant, 'Participant', 'Number of messages', 'msg_per_participants.png')
 
+    msg_per_participant = sorted(conv.number_of_char_per_participants.items(), key=operator.itemgetter(1), reverse=True)
+    #create_bar_plot_from_list(msg_per_participant, 'Participant', 'Number of characters', 'char_per_participants.png')
 
-       
+    per_year_sorted = sorted(conv.get_number_of_messages_per_year().items())
+    #create_bar_plot_from_list(per_year_sorted, 'Year', 'Number of messages', 'year.png')
+
+    ### Week day is a special case 
+    per_weekday = conv.get_number_of_messages_per_weekday()
+    weekday_ordered = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    weekday_value = []
+    for day in weekday_ordered:
+        weekday_value.append(per_weekday[day])
+    create_bar_plot(weekday_ordered, 'Weekday', weekday_value, 'Number of messages', 'weekday.png')
+
+    per_hour_sorted = sorted(conv.get_number_of_messages_per_hour().items())
+    #create_bar_plot_from_list(per_hour_sorted, 'Hours', 'Number of messages','hour.png')
+
+    images_path = ['msg_per_participants.png', 'char_per_participants.png', 'year.png', 'weekday.png', 'hour.png']
+    #merge_pictures(images_path)
+
+    #print_dict_ordered_reverse(conv.number_of_char_per_participants)
+    #print_dict_ordered_reverse(conv.number_of_photos_per_participants)
+    
